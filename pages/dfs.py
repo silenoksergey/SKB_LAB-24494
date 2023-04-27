edbf0a8fcf9a9ee6fdad7697afacf39c09afce57
Sergey Silenok <silenoksergey34@gmail.com>
2023-04-27 21:47:04 +0500

    Убрал файлы IDE

--- pages/dfs.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def generate_cyrillic_symbol(length):
-    letters = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдежзийклмнопрстуфхцчшщъыьэюяЁёabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    random_letters = letters[random.randrange(length)]
-    return random_letters
-
-
